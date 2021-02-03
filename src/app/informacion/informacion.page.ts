266e21a7f3a2c8bfca5d92df23ea5d022b199d92
Noé <alum.noe.garcia.ramirez@ieslosremedios.org>
2021-02-03 09:46:52 +0100

    Implementado imagePicker, sin la parte html

--- src/app/informacion/informacion.page.ts
+++ src/app/informacion/informacion.page.ts
@@ -4,6 +4,8 @@ import { Articulo } from '../articulo';
 import { FirestoreService } from '../firestore.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 @Component({
   selector: 'app-informacion',
@@ -16,7 +18,10 @@ export class InformacionPage implements OnInit {
   constructor(private activatedRoute: ActivatedRoute,
     private firestoreService: FirestoreService,
     private router: Router,
-    private alertCtrl: AlertController) { }
+    private alertCtrl: AlertController,
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private imagePicker: ImagePicker) { }
 
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
@@ -30,6 +35,81 @@ export class InformacionPage implements OnInit {
 
   articuloEditando: Articulo;
   idArticuloSelec: string;
+
+  async uploadImagePicker() {
+    //Mensaje de espera
+    const loading = await this.loadingController.create({
+      message: 'Por favor espere...'
+    });
+
+    //Mensaje de finalización de subida de imagen
+    const toast = await this.toastController.create({
+      message: 'Imagen subida con éxito',
+      duration: 2000
+    });
+
+    //Comprobar si la aplicación tiene permisos de lectura
+    this.imagePicker.hasReadPermission().then(
+      (result) => {
+        //Si no tiene permiso de lectura se solicita al usuario
+        if(result == false) {
+          this.imagePicker.requestReadPermission();
+        } else {
+          //Abrir selector de imágenes
+          this.imagePicker.getPictures({
+            maximumImagesCount: 1,    //Permite 1 imagen
+            outputType: 1             // 1 = Base 64
+          }).then(
+            (results) => {
+              //En la variable results se tienen las imágenes seleccionadas
+              let nombreCarpeta = "imagenes";
+              //Recorrer todas las imágenes que haya seleccionado el usuario
+              //Aunque realmente sólo será 1 como se ha indicado en las opciones
+              for (var i = 0; i < results.length; i++) {
+                //Muestra mensaje de espera
+                loading.present();
+                //Assignar nombre de imagen en función de la hora actual para
+                //evitar duplicidades de nombres
+                let nombreImagen = `${new Date().getTime()}`;
+                //Llamar al método que sube la imagen al Storage
+                this.firestoreService.uploadImage(nombreCarpeta, nombreImagen,
+                results[i])
+                .then(snapshot => {
+                  snapshot.ref.getDounloadURL()
+                  .then(downloadURL => {
+                    //En la variable downloadURL se tiene la dirección de descarga
+                    //de la imagen
+                    console.log("downloadURL:" + downloadURL);
+                    //Mostrar el mensaje de finalización de subida
+                    toast.present();
+                    //Ocultar mensaje de espera
+                    loading.dismiss();
+                  })
+                })
+              }
+            },
+            (err) => {
+              console.log(err)
+            }
+          );
+        }
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  async deleteFile(fileURL) {
+    const toast = await this.toastController.create({
+      message: 'Archivo eliminado correctamente',
+      duration: 2000
+    });
+    this.firestoreService.deleteFileFromURL(fileURL)
+    .then(() => {
+      toast.present();
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
   async alertConfirmar() {
     const alert = await this.alertCtrl.create({
